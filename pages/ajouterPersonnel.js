521e245b27f0e7d342a6a4354d7c874353e5981f
wilfried <wilfrieddonfouekeng11@gmail.com>
2022-11-10 18:12:53 +0100

    Creation des pages personnel et ajouterPersonnel

---
+++ pages/ajouterPersonnel.js
@@ -0,0 +1,7 @@
+export default function ajouterPersonnel() {
+  return (
+    <div className="ajouterPersonnel">
+      <h1>Ajouter Personnel</h1>
+    </div>
+  );
+}
---
+++ pages/personnel.js
@@ -0,0 +1,7 @@
+export default function Personnel() {
+  return (
+    <div className="personnel">
+      <h1>Personnel</h1>
+    </div>
+  );
+}
