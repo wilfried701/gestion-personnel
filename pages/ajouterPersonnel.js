43a7ab47fea40e54a197519a5ba0402445a85bc8
wilfried <wilfrieddonfouekeng11@gmail.com>
2022-11-11 16:34:52 +0100

    Ajouter une personne dans la base de donne su firestore

--- pages/ajouterPersonnel.js
+++ pages/ajouterPersonnel.js
@@ -1,26 +1,42 @@
-import { Input } from '@angular/core'
-import React from 'react'
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import { useRouter } from "next/router";
+import React from "react";
+import { db } from "../firebase/config";
 
-export default function ajouterPersonnel () {
-  const [nom, setNom] = React.useState('')
-  const [prenom, setPrenom] = React.useState('')
-  const [fonction, setFonction] = React.useState('')
-  const [email, setEmail] = React.useState('')
-  const [telephone, setTelephone] = React.useState('')
-  const [joursdetravail, setJourDeTravail] = React.useState('')
+export default function ajouterPersonnel() {
+  const [nom, setNom] = React.useState("");
+  const [prenom, setPrenom] = React.useState("");
+  const [fonction, setFonction] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [telephone, setTelephone] = React.useState("");
+  const [joursdetravail, setJourDeTravail] = React.useState("");
+  const router = useRouter();
 
+  async function ajouterPersonne(event) {
+    event.preventDefault();
+    const data = {
+      nom: nom,
+      prenom: prenom,
+      fonction: fonction,
+      email: email,
+      numero: telephone,
+      status: "absent",
+    };
+    await addDoc(collection(db, "personnel"), data);
+    router.push("/");
+  }
   return (
-    <div className='ajouterPersonnel'>
+    <div className="ajouterPersonnel">
       <h1>Ajouter un membre</h1>
       <div>
-        <form>
-          <div className='nom'>
+        <form onSubmit={ajouterPersonne}>
+          <div className="nom">
             <label>Nom</label>
             <input
-              name='nom'
-              type='name'
+              name="nom"
+              type="name"
               value={nom}
-              onChange={e => setNom(e.target.value)}
+              onChange={(e) => setNom(e.target.value)}
               required
             />
           </div>
@@ -28,38 +44,38 @@ export default function ajouterPersonnel () {
           <label>
             Prenom
             <input
-              name='prenom'
-              type='name'
+              name="prenom"
+              type="name"
               value={prenom}
-              onChange={e => setPrenom(e.target.value)}
+              onChange={(e) => setPrenom(e.target.value)}
               required
             />
           </label>
           <label>Fonction</label>
           <input
-            name='fonction'
-            type='name'
+            name="fonction"
+            type="name"
             value={fonction}
-            onChange={e => setFonction(e.target.value)}
+            onChange={(e) => setFonction(e.target.value)}
             required
           />
           <label>
             Email
             <label>
               <input
-                name='email'
-                type='email'
+                name="email"
+                type="email"
                 value={email}
-                onChange={e => setEmail(e.target.value)}
+                onChange={(e) => setEmail(e.target.value)}
                 required
               />
               Telephone
             </label>
             <input
-              name='telephone'
-              type='number'
+              name="telephone"
+              type="number"
               value={telephone}
-              onChange={e => setTelephone(e.target.value)}
+              onChange={(e) => setTelephone(e.target.value)}
               required
             />
           </label>
@@ -67,93 +83,91 @@ export default function ajouterPersonnel () {
           <div>
             <h4>Jours de travail</h4>
 
-            <div className='jours'>
-              <label> 
+            <div className="jours">
+              <label>
                 Lundi
-                <input 
-                  type="checkbox" 
-                  name="lundi" 
-                  onChange={e => setJourDeTravail(e.target.value)}
+                <input
+                  type="checkbox"
+                  name="lundi"
+                  onChange={(e) => setJourDeTravail(e.target.value)}
                 />
               </label>
-              <label> 
+              <label>
                 Mardi
-                <input 
-                  type="checkbox" 
-                  name="mardi" 
-                  onChange={e => setJourDeTravail(e.target.value)}
+                <input
+                  type="checkbox"
+                  name="mardi"
+                  onChange={(e) => setJourDeTravail(e.target.value)}
                 />
               </label>
-              <label> 
+              <label>
                 Mercredi
-                <input 
-                  type="checkbox" 
-                  name="mercredi" 
-                  onChange={e => setJourDeTravail(e.target.value)}
+                <input
+                  type="checkbox"
+                  name="mercredi"
+                  onChange={(e) => setJourDeTravail(e.target.value)}
                 />
-              </label> 
-              <label> 
+              </label>
+              <label>
                 Jeudi
-                <input 
-                  type="checkbox" 
-                  name="mercredi" 
-                  onChange={e => setJourDeTravail(e.target.value)}
+                <input
+                  type="checkbox"
+                  name="mercredi"
+                  onChange={(e) => setJourDeTravail(e.target.value)}
                 />
-              </label>      
-
+              </label>
             </div>
 
-             <div className='jour'>
-              <label> 
+            <div className="jour">
+              <label>
                 Vendredi
-                <input 
-                  type="checkbox" 
-                  name="lundi" 
-                  onChange={e => setJourDeTravail(e.target.value)}
+                <input
+                  type="checkbox"
+                  name="lundi"
+                  onChange={(e) => setJourDeTravail(e.target.value)}
                 />
               </label>
-              <label> 
+              <label>
                 Samedi
-                <input 
-                  type="checkbox" 
-                  name="mardi" 
-                  onChange={e => setJourDeTravail(e.target.value)}
+                <input
+                  type="checkbox"
+                  name="mardi"
+                  onChange={(e) => setJourDeTravail(e.target.value)}
                 />
               </label>
-              <label> 
+              <label>
                 Dimanche
-                <input 
-                  type="checkbox" 
-                  name="mercredi" 
-                  onChange={e => setJourDeTravail(e.target.value)}
+                <input
+                  type="checkbox"
+                  name="mercredi"
+                  onChange={(e) => setJourDeTravail(e.target.value)}
                 />
-              </label>     
-
-            </div>                   
+              </label>
+            </div>
           </div>
-
 
           <div>
             <h4>Congés</h4>
           </div>
 
-          <div className='conges'>
+          <div className="conges">
             <p>du</p>
-            <input type='date' />
+            <input type="date" />
           </div>
 
-          <div className='conges2'>
+          <div className="conges2">
             <p>au</p>
-            <input type='date' />
+            <input type="date" />
           </div>
 
           <div>
-            <button className="creer">Creer</button>
+            <button className="creer" type="submit">
+              Creer
+            </button>
             <button className="annuler">Annuler</button>
           </div>
-
         </form>
       </div>
     </div>
-  )
+  );
 }
